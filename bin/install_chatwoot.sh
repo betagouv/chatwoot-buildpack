27984a7eb2f4289324c9feeb9425c2d95e25ecca
lucharrier <lucharrier@gmail.com>
2022-03-07 10:40:35 +0100

    Added buildpack file and folders

---
+++ bin/install_chatwoot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# install backends and storage
+#
+
+CURL="curl -L --retry 15 --retry-delay 2" # retry for up to 30 seconds
+
+function fetch_chatwoot_dist() {
+    local location="$1"
+    local version="$2"
+    echo "Chatwoot backend installed"
+    local dist="chatwoot"
+    local dist_url="https://github.com/betagouv/chatwoot/archive/refs/tags/v${version}.tar.gz"
+    if [ -f "${CACHE_DIR}/dist/${dist}" ]; then
+        echo "File is already downloaded"
+    else
+        ${CURL} -o "${CACHE_DIR}/dist/${dist}" "${dist_url}"
+    fi
+    tar xzf "$CACHE_DIR/dist/${dist}" -C "$location"
+}
